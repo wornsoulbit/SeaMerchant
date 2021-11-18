ed1bdab1ca6dcd0901997c34b801eeea5bba9876
Ebrahim Vericain <evericain01@hotmail.com>
2021-11-18 12:41:38 -0500

    Made death screen scene

--- Assets/Scripts/PlayerHealthController.cs
+++ Assets/Scripts/PlayerHealthController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerHealthController : MonoBehaviour
 {
@@ -24,6 +25,10 @@ public class PlayerHealthController : MonoBehaviour
 				{
 					TakeDamage(20);
 				}*/
+		if(currentHealth == 0)
+        {
+
+        }
 	}
 
 	public void TakeDamage(int damage)

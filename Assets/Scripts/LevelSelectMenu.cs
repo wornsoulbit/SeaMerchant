d26736efce26fda9ed6f15679f94d4216f8cabf1
NicolasLaurin <54424767+NicolasLaurin@users.noreply.github.com>
2021-11-26 08:32:57 -0500

    Added Main Menu compatibility with the rest of the game

--- Assets/Scripts/LevelSelectMenu.cs
+++ Assets/Scripts/LevelSelectMenu.cs
@@ -4,17 +4,21 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LevelSelectMenu : MonoBehaviour {
+    const int LEVEL_1_BUILD_INDEX = 1;
+    const int LEVEL_2_BUILD_INDEX = 2;
+    const int LEVEL_3_BUILD_INDEX = 3;
+
     public void StartLevel1() {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(LEVEL_1_BUILD_INDEX);
     }
 
     public void StartLevel2()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+        SceneManager.LoadScene(LEVEL_2_BUILD_INDEX);
     }
 
     public void StartLevel3()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+        SceneManager.LoadScene(LEVEL_3_BUILD_INDEX);
     }
 }

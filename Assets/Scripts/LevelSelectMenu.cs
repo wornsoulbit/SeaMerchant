1512f6060e22a26f0d0d81df54e95ae785be5e51
wornsoulbit <alexandervasil840@gmail.com>
2021-11-30 11:43:11 -0500

    Added Loading Screen, Screen transistions.

--- Assets/Scripts/LevelSelectMenu.cs
+++ Assets/Scripts/LevelSelectMenu.cs
@@ -2,23 +2,62 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelSelectMenu : MonoBehaviour {
     const int LEVEL_1_BUILD_INDEX = 2;
     const int LEVEL_2_BUILD_INDEX = 3;
     const int LEVEL_3_BUILD_INDEX = 4;
 
+    public Animator transition;
+    public Animator musicTransition;
+
+    public float transistionTime = 1f;
+
+    public GameObject LoadingScreen;
+    public Slider slider;
+
     public void StartLevel1() {
-        SceneManager.LoadScene(LEVEL_1_BUILD_INDEX);
+        StartCoroutine(LoadLevel(LEVEL_1_BUILD_INDEX));
+        Debug.Log("Loading Level 1");
     }
 
     public void StartLevel2()
     {
-        SceneManager.LoadScene(LEVEL_2_BUILD_INDEX);
+        StartCoroutine(LoadLevel(LEVEL_2_BUILD_INDEX));
     }
 
     public void StartLevel3()
     {
-        SceneManager.LoadScene(LEVEL_3_BUILD_INDEX);
+        StartCoroutine(LoadLevel(LEVEL_3_BUILD_INDEX));
+    }
+
+    public void LoadNextLevel()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    public void LoadMainMenu()
+    {
+        StartCoroutine(LoadLevel(0));
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        transition.SetTrigger("Start");
+        Debug.Log("Starting Level Transisition");
+        /*musicTransition.SetTrigger("Start");*/
+
+        yield return new WaitForSeconds(transistionTime);
+
+        AsyncOperation operation = SceneManager.LoadSceneAsync(levelIndex);
+
+        LoadingScreen.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            slider.value = operation.progress;
+            yield return null;
+        }
     }
 }
